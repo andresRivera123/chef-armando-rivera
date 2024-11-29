11ca4ce26b0eb9f007c7fff64b2d06086252225d
Andyk <carlosandresriveravalle@gmail.com>
2024-11-30 00:37:52 +0100

    Create Hero and Card list + Logic print cards

---
+++ src/utils/Cards.ts
@@ -0,0 +1,32 @@
+export interface Card {
+    id: number;
+    img: string;
+    country: string;
+    date: string;
+    title: string;
+}
+
+export const CARDS: Card[] = [
+    {
+        id: 1,
+        img: "/richtershof-alemania-cover-card.png",
+        country: "Marruecos",
+        date: "2024-11-25",
+        title: "Sabores de Marruecos",
+      },
+      {
+        id: 2,
+        img: "/richtershof-alemania-cover-card.png",
+        country: "Marruecos",
+        date: "2024-11-25",
+        title: "Sabores de Marruecos",
+      },
+      {
+        id: 3,
+        img: "/richtershof-alemania-cover-card.png",
+        country: "Marruecos",
+        date: "2024-11-25",
+        title: "Sabores de Marruecos",
+      },
+
+]
